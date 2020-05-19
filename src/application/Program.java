b2056dd9338a5aa125aa7a5ef5cd6993b9b68938
ribeirojunior <sergio.rivusjr@gmail.com>
2020-05-19 15:05:01 -0300

    Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Chess Aplication");
+		Position pos = new Position(4, 6);
+		System.out.println(pos);
 
 	}
 
